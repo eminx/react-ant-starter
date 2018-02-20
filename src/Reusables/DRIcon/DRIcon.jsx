dc61a2f0c4dc3cdbf9bba7cc46e7da7c3231ae5c
Emin Durak <emin@pomegra.org>
2018-02-20 23:32:40 +0100

    svg icon stuff

--- src/Reusables/DRIcon/DRIcon.jsx
+++ src/Reusables/DRIcon/DRIcon.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DRIcon = props => (
-  <svg width="22" height="22" viewBox="0 0 1024 1024">
-    <path d={props.icon}></path>
-  </svg>
-);
+const DRIcon = props => {
+  const styles = {
+    svg: {
+      display: 'inline-block',
+      verticalAlign: 'middle',
+    },
+    path: {
+      fill: props.color,
+    },
+  };
+
+  return (
+    <svg
+      style={styles.svg}
+      width={`${props.size}px`}
+      height={`${props.size}px`}
+      viewBox="0 0 1024 1024"
+    >
+      <path
+        style={styles.path}
+        d={props.icon}
+      ></path>
+    </svg>
+  );
+};
 
 DRIcon.propTypes = {
   icon: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
+
+DRIcon.defaultProps = {
+  size: 24,
 };
 
 export default DRIcon;
